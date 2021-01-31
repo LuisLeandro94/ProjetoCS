2da799ad43a000f5a7672ffcc15e9abf5d9606de
Marco Oliveira <fermandesmarco@gmail.com>
2021-01-31 02:16:04 +0000

    Venda, falta alterar para receber pagamento, dar troca etc

--- Supermercado/Supermercado/Menus/Caixa.cs
+++ Supermercado/Supermercado/Menus/Caixa.cs
@@ -1,6 +1,8 @@
 ﻿using Supermercado.Data;
 using Supermercado.Menus;
 using System;
+using System.Collections.Generic;
+
 namespace Supermercado
 {
     public class Caixa : Funcionário
@@ -47,7 +49,8 @@ namespace Supermercado
                 switch (escolha)
                 {
                     case 1:
-                        GestorProdutos.EscreverListaConsola();
+                        //VendaAlt();
+                        EscolhaEditar();
                         break;
                     case 2:
                         //LISTAR FATURA
@@ -67,5 +70,119 @@ namespace Supermercado
                 Console.Clear();
             }
         }
+
+        public static void VendaAlt(string productName)
+        {
+            GestorProdutos.EscreverListaConsola();
+            Console.Write("Cliente: ");
+            string nomeCliente = Console.ReadLine();
+
+            Console.Write("\nNome do Produto:");
+            string nomeProduto = Console.ReadLine();
+
+            Console.Write("\nQuantidade");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            
+            for (int i = 0; i < GestorProdutos.listaProdutos.Count; i++)
+            {
+
+                if (GestorProdutos.listaProdutos[i].productName.ToLower().Equals(nomeProduto.ToLower()))
+                {
+
+                    GestorProdutos.listaProdutos[i].stock = GestorProdutos.listaProdutos[i].stock - quantidade;
+                }
+                
+            }
+           
+
+        }
+
+
+
+        #region Vender Produto  ---ALTERAR--- 
+        public static void EscolhaEditar()
+        {
+            GestorProdutos.EscreverListaConsola();
+            Console.WriteLine("Cliente: ");
+            string nomeCliente = Console.ReadLine();
+
+            Console.WriteLine("Produto para compra: ");
+            string produtocompra = Console.ReadLine();
+
+            Console.Write("Quantidade a comprar: ");
+            double quantidade = Convert.ToDouble(Console.ReadLine());
+
+
+
+            EditaaContact(nomeCliente, produtocompra, quantidade);
+            //GravarProduto();
+            int indexAremover;
+            double valor;
+            for (int i = 0; i < GestorProdutos.listaProdutos.Count; i++)
+            {
+
+                if (GestorProdutos.listaProdutos[i].productName.ToLower().Equals(produtocompra.ToLower()))
+                {
+                    indexAremover = i;
+                    valor = Convert.ToDouble(GestorProdutos.listaProdutos[i].unitPrice) * quantidade;
+                    Console.WriteLine("------------FATURA------------");
+                    Console.WriteLine("Compra: {0} * Quantidade: {1} ", produtocompra, quantidade);
+                    Console.WriteLine("Valor da compra: {0}",valor);
+                }
+            
+            }
+            Console.WriteLine("Pagamento: ");
+
+            //A CONTINUAR
+           
+
+        }
+        static public Produtos EditaaContact(string nomeCliente, string nome, double novoStock)
+        {
+
+
+            Produtos contactoAEditar = FindContact(nome);
+
+            if (contactoAEditar != null)
+            {
+               
+                if (!novoStock.Equals(""))
+                {
+                    contactoAEditar.stock = contactoAEditar.stock - novoStock;
+                }
+
+                return contactoAEditar;
+
+            }
+
+            return null;
+        }
+        static public Produtos FindContact(string productName)
+        {
+            foreach (Produtos p in GestorProdutos.listaProdutos)
+            {
+                if (p.productName.ToLower().Equals(productName.ToLower()))
+                {
+
+                    return p;
+                }
+            }
+
+            return null;
+
+
+        }
+
+        static public void Pagamento()
+        {
+            
+
+        }
+        #endregion
+
+
     }
 }
